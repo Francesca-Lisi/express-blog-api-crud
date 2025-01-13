5a51c53eb2d73f75d47c70dde0cd213f674481a4
Francesca-Lisi <francescalisi2143@gmail.com>
2025-01-13 17:15:06 +0100

    feat: add app.get and app.listen and check on thunderclient

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const posts = require('./data/posts');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server dei post')
+})
+
+app.listen(port, () => {
+  console.log('sono in ascolto')
+})
+
 
 console.log(posts)
